0754833061266c6571c081a11176ce345506c544
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-19 19:48:45 -1000

    created tests for word checking and converting word

--- test/pigLatinTranslator_spec.js
+++ test/pigLatinTranslator_spec.js
@@ -10,5 +10,21 @@ describe('pigLatinTranslator', function() {
   it('should return an object when invoked', function(){
     expect(pigLatinTranslator).to.be.an('object');
   });
+  describe('word checker', function() {
+    it('should check if word starts with A,E,I,O,U', function (){
+      expect(piglatinTranslator.wordCheck).to.be.an('function');
+      expect(pigLatinTranslator.wordCheck('eat')).to.equal(true);
+            expect(pigLatinTranslator.wordCheck('pig')).to.equal(false);
+    });
+  });
+  describe('word conversion', function(){
+    it('should convert word to correct word in pig latin', function(){
+      expect(pigLatinTranslator.convert).to.be.an('function');
+      expect(pigLatinTranslator.convert('eat')).to.equal('eatay');
+      expect(pigLatinTranslator.convert('omelet')).to.equal('omeletay');
+      expect(pigLatinTranslator.convert('pig')).to.equal('ig-pay');
+      expect(pigLatinTranslator.convert('banana')).to.equal('anana-bay');
+    });
+  });
 
 });
